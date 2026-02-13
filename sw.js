a8d8c801c5abe304effe8c15af8700c580ed6db9
cmegcloud <cmexpertsgroup@gmail.com>
2026-02-14 03:55:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-// Dummy Service Worker to satisfy PWA requirements
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Installed');
 });
 
 self.addEventListener('fetch', (e) => {
-  // Pass through all requests normally
 });
